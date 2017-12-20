36e2e1b2202693cfe610f7ee157b566486938516
Fenrir2539 <Fenrir2539@users.noreply.github.com>
2017-12-20 10:15:30 -0500

    Done with Pop Quiz
    ReverseString.java: Reversed a string by turning it into a char array and walking through backwards adding one char at a time to a stringbuilder.
    Travis.yml: modified as per slack.

--- src/main/java/quiz/ReverseString.java
+++ src/main/java/quiz/ReverseString.java
@@ -1,7 +1,25 @@
 package quiz;
 
+/*
 public class ReverseString {
     public String reverse(String input) {
         return new StringBuffer(input).reverse().toString();
     }
+*/
+
+public class ReverseString {
+	public String reverse(String input) {
+		//Turn input string into an easily traversable char array
+		char[] ogString = input.toCharArray();
+		//A String Builder to make the reversed string one char at a time
+		StringBuilder reversedStringB = new StringBuilder();
+		//Start at the end of the char array (length-1) place each char into StrBuilder
+		for (int i = ogString.length-1; i>=0; i--) {
+			reversedStringB.append(ogString[i]);
+		}
+		//Turn the Builder into a string
+		String output = reversedStringB.toString();
+		return output;
+	}
+
 }
